060532ad6ba49635d7531068afbc8e3c0426ad05
Moti <motisa94@gmail.com>
2024-03-27 09:22:00 +0100

    feat: add post login function for admins

--- api/administradores/administradores_controlador.js
+++ api/administradores/administradores_controlador.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {body, param, validationResult} from 'express-validator'
+import { body, param, validationResult } from 'express-validator'
 import administradoresMysql from './administradores_mysql.js';
 
 var router = express.Router();
@@ -20,6 +20,10 @@ let reglasAdminId = [
 let reglasAdminPut = [
     body('adminId').notEmpty()
 ]
+const loginValidationRules = [
+    body('usuario').notEmpty(),
+    body('password').notEmpty(),
+];
 
 //usamos async para poder usar en su interior instrucciones tipo wait.
 // Ruta para crear un nuevo admin
@@ -47,7 +51,7 @@ router.get('/', async (req, res, next) => {
         // Obtiene todos admins de la base de datos
         let admins = await administradoresMysql.getAdminsMsql();
         // Devuelve admins obtenidos
-        res.json(admins); 
+        res.json(admins);
     } catch (error) {
         next(error)
     }
@@ -104,5 +108,32 @@ router.delete('/:id', reglasAdminId, async (req, res, next) => {
         next(error)
     }
 });
+
+// Ruta para la autenticación de usuarios administradores
+router.post("/login", loginValidationRules, async (req, res) => {
+    try {
+        // Valida los errores de la solicitud
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            // Si hay errores, devuelve un mensaje de error con los detalles
+            return res.status(400).json({ errors: errors.array() });
+        }
+        // Obtiene el usuario y la contraseña del cuerpo de la solicitud
+        const { usuario, password } = req.body;
+        // Intenta iniciar sesión con las credenciales proporcionadas
+        const admin = await administradoresMysql.postLogin(usuario, password);
+        if (admin) {
+            // Si el inicio de sesión tiene éxito, devuelve los datos del administrador
+            return res.json(admin);
+        } else {
+            // Si las credenciales son incorrectas, devuelve un mensaje de error
+            return res.status(401).json({ error: 'Nombre de usuario y contraseña incorrectos' });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
+
 
 export default router;
