56ed033af55dd07fe819b7d64da71a814dde35d7
Moti <motisa94@gmail.com>
2024-03-27 11:23:27 +0100

    feat: add consult version controlador

---
+++ api/version/version_controlador.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+const router = Router();
+import pk from '../../package.json' assert {type: 'json'};
+
+router.get('/', (req, res) => {
+    let version = {
+        name: pk.name,
+        version:pk.version,
+        description: pk.description
+    };
+    res.json(version);
+})
+export default router;
