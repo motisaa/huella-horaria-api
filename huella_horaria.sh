d616b204255a4a4dcb180546e01cf162967ae0e6
Moti <109862614+motisaa@users.noreply.github.com>
2024-04-26 09:16:47 +0200

    Update huella_horaria.sh

--- huella_horaria.sh
+++ huella_horaria.sh
@@ -1,11 +1,11 @@
 docker rm -f huellahoraria
 docker pull motisa/huellahoraria:latest 
 docker run -d --name huellahoraria -p10088:8080 \
--e PORT=8080 \
+-e PORT= \
 -e MYSQL_HOST=mysql01.ariadnasw.com \
 -e MYSQL_PORT=3306 \
--e MYSQL_USER=moti \
--e MYSQL_PASSWORD=holaSoyMoti123 \
+-e MYSQL_USER= \
+-e MYSQL_PASSWORD= \
 -e MYSQL_DATABASE=huella_horaria \
 -e WINSTON_FILELEVEL=info \
 -e WINSTON_CONSOLELEVEL=debug \
