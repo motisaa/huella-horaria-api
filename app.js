d8471ad550890578e6bb0e116143ba2edf498206
Moti <motisa94@gmail.com>
2024-05-08 12:07:22 +0200

    feat: update release

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ import fichajesRouter from './api/fichajes/fichajes_controlador.js';
 import gruposRouter from './api/grupos/grupos_controlador.js';
 import versionRouter from './api/version/version_controlador.js';
 import loginRouter from './api/login/login_controlador.js'
+import auth from './api/middleware/auth.js';
 
 // FIXED de chatGPT: utilizamo el módulo path de Node.js para obtener el directorio actual
 // porque variable interno __dirname no está definido en el ámbito de los módulos de ES.
@@ -21,6 +22,7 @@ const appServer = {
     createServer: () => {
         winston.info('crear servidor');
         app.use(cors());
+        
         /*
         La opción extended se establece en false, lo que significa que
         solo se analizarán los datos que no estén en forma de objeto o matriz.
@@ -32,7 +34,7 @@ const appServer = {
         app.use(bodyParser.json());
         // desde www sirve archivos
         app.use(express.static(__dirname + '/www'));
-
+        
         // fixed: las rutas de api tienen que ir después de bodyParser
         app.use('/v1/empresas', empresasRouter);
         app.use('/v1/administradores', adminsRouter);
@@ -42,6 +44,7 @@ const appServer = {
         app.use('/v1/login', loginRouter);
         //adding router of group
         app.use('/v1/grupos_trabajadores', gruposRouter);
+        
         // cuando las llamadas no son de api sirves el fichero index.html del directorio www
         app.use((req, res, next) => {
             res.sendFile(path.join(__dirname, "www", "index.html"));
